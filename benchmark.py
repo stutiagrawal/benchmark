c73a8258b4b1ca8caa5bf14ca9af8eacfd25d7fa
Ubuntu <ubuntu@gatk-benchmark-2.novalocal>
2014-09-16 19:26:51 +0000

    added checks

--- benchmark.py
+++ benchmark.py
@@ -149,15 +149,21 @@ if __name__ == "__main__":
         line = line.split("\t")
         uuid = line[16]
         dirname = os.path.join(args.data, uuid)
+        has_fastq_files = False
         if not os.path.isdir(dirname):
                 os.makedirs(dirname)
                 download_bam(args.gt_path, uuid, dirname, args.cghub_key)
         if(os.path.isdir(dirname)):
             bamfile = getBAM(os.path.join(dirname, uuid))
             if(os.path.isfile(bamfile)):
-                convert_to_fastq(dirname, bamfile, uuid, args.picard)
-                if(os.path.isfile('%s_1.fastq.gz' %(uuid)) and os.path.isfile('%s_2.fastq.gz' %(uuid))):
+                if(os.path.isfile('%s_1.fastq.gz' %(os.path.join(dirname, uuid, uuid))) 
+                   and os.path.isfile('%s_2.fastq.gz' %(os.path.join(dirname, uuid, uuid)))):
+                    has_fastq_files = True
+                if(has_fastq_files):
                     align_bwa(dirname, uuid, args.ref, args.bwa_path, bamfile)
+                else:
+                    convert_to_fastq(dirname, bamfile, uuid, args.picard)
+
             #GATK_snp_calling(args.ref, bamfile, dirname, "UnifiedGenotyper",
             #                args.GATK)
             #GATK_snp_calling(args.ref, bamfile, dirname, "HaplotypeCaller",
